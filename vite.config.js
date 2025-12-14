cd4cee25ec345fb45987e434c9455c225506f158
iniraply <prapthorespect@gmail.com>
2025-12-15 05:31:27 +0700

    Force react version resolution using overrides

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,4 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   // TAMBAHKAN BAGIAN INI:
-  resolve: {
-    dedupe: ["react", "react-dom"], // <--- Ini memaksa Vite pakai 1 versi React saja
-  },
 });
