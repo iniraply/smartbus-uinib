94f99ad61f69657dab8e1dd951b6e1e23293f81a
iniraply <prapthorespect@gmail.com>
2025-12-14 07:49:09 +0700

    refactor: update dependencies and configuration for React 18 and Tailwind CSS 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,18 @@
-// tailwind.config.js
-
-// 1. Impor tema default di bagian atas
-const defaultTheme = require("tailwindcss/defaultTheme");
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
-      // 2. Tambahkan/modifikasi properti fontFamily
+      colors: {
+        brand: {
+          dark: "#3E0703", // Coklat Sangat Gelap (Teks/Sidebar)
+          primary: "#660B05", // Merah Maroon (Header)
+          accent: "#8C1007", // Merah Terang (Tombol/Icon Aktif)
+          cream: "#FFF0C4", // Krem (Background)
+        },
+      },
       fontFamily: {
-        sans: ["Poppins", ...defaultTheme.fontFamily.sans],
+        sans: ["Poppins", "sans-serif"],
       },
     },
   },
